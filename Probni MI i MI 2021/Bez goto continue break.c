c843d7719ee25e363f0ddcc7f67fa89c0a42bc61
Dado Skrgic <dadoskrgic@gmail.com>
2022-11-19 20:29:59 +0100

    Update Bez goto continue break.c

--- Probni MI i MI 2021/Bez goto continue break.c
+++ Probni MI i MI 2021/Bez goto continue break.c
@@ -1,9 +1,9 @@
 z = x;
 do{
-   z += w;
-	i = i -t;
-	if (i < m) k += 10;
+    z += w;
+    i = i -t;
+    if (i < m) k += 10;
    
-}while(z >= 200 && i - t < m);
+} while(z >= 200 && i - t < m);
 if (i >= m) k += 100;
 z *= 1000;

e901327edabc184307e6e838b7b0dd56c0c7453a
Dado Skrgic <dadoskrgic@gmail.com>
2022-11-19 18:20:29 +0100

    Update Prvo i zadnje slovo rijeci veliko.c

--- Probni MI i MI 2021/Prvo i zadnje slovo rijeci veliko.c
+++ Probni MI i MI 2021/Prvo i zadnje slovo rijeci veliko.c
@@ -5,6 +5,7 @@ int main() {
    char niz[11];
    printf("Unesite niz znakova > ");
    fgets(niz, 11, stdin);
+   
    printf("Rezultat: ");
    for (int i = 0; niz[i] != '\0' && niz[i] != '\n' && i < 11; i++) {
       if (i == 0 || (i > 0 && niz[i - 1] == ' ') || (niz[i + 1] == ' ' || niz[i + 1] == '\n' || niz[i + 1] == '\0')) {

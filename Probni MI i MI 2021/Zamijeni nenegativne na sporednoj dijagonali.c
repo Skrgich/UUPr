058a818f6f64f52337a977ef57e9863e5ff47736
Dado Skrgic <dadoskrgic@gmail.com>
2022-11-19 18:22:12 +0100

    Update Zamijeni nenegativne na sporednoj dijagonali.c

--- Probni MI i MI 2021/Zamijeni nenegativne na sporednoj dijagonali.c
+++ Probni MI i MI 2021/Zamijeni nenegativne na sporednoj dijagonali.c
@@ -9,27 +9,18 @@ int main() {
    }while (n < 1);
 
    int polje[n][n];
+   
    printf("Rezultat:");
    for (int i = 0; i < n; i++){
+      
       printf("\n");
       for (int j = 0; j < n; j++){
          scanf("%d", &polje[i][j]);
+         
          polje[i][j] = i + j + 1 != n ? polje[i][j] : i % 2 == 0 ? 0 : 1;
+         
          printf("%3d", polje[i][j]);
       }
    }
-
-
    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-

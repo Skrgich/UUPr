4d5a49c5a735afe0d25798970268ad464f931bab
Dado Skrgic <dadoskrgic@gmail.com>
2022-11-19 18:18:41 +0100

    Update Najdulja rijec.c

--- Probni MI i MI 2021/Najdulja rijec.c
+++ Probni MI i MI 2021/Najdulja rijec.c
@@ -5,23 +5,25 @@ int main() {
    char niz[51];
    printf("Niz > ");
    fgets(niz, 51, stdin);
-   int rpoc;
-   int maxpoc;
-   int max_r = 0;
-   int i = 0;
-   int br = 0;
+   
+   int rpoc, maxpoc, max_r = 0, i = 0, br = 0;
+
    while (niz[i] != '\n') {
       rpoc = i;
+      
       while (niz[i] != '.' && niz[i] != ' ')
          i++;
       br++;
+      
       if (i - rpoc > max_r) {
          max_r = i - rpoc;
          maxpoc = rpoc;
       }
       i++;
    }
+   
    printf("Broj rijeci: %d\n", br);
+   
    printf("Najdulja rijec: ");
    for (int i = maxpoc; i < maxpoc + max_r; i++)
       printf("%c", niz[i]);

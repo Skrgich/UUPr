298882076e0d44a3d37e7e4c436d2dacbde601fc
Dado Skrgic <dadoskrgic@gmail.com>
2022-12-16 17:02:44 +0100

    .

--- pripreme_za_labose_2022/6/PovecajKonvertiraj.c
+++ pripreme_za_labose_2022/6/PovecajKonvertiraj.c
@@ -3,6 +3,5 @@
 int PovecajKonvertiraj(float *kune, int bonus_kune, float *postotak) {
    *postotak = 100. * bonus_kune / *kune;
    *kune = (*kune + (float)bonus_kune) / 7.5345;
-   if (*postotak >= 10.)
-      printf("OBAVIJESTITI POREZNU!");
+   return *postotak >= 10. ? 1 : 0;
 }
